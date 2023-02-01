965b89043037c723f939a000dd6c56b7573e8913
Ayush <Ayushmittal017@gmail.com>
2023-02-01 17:22:56 +0530

    all

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './App.scss';
 import SliderComponent from './component/slider/SliderComponent';
 import Navigate from './NavigateFile/Navigate';
@@ -7,6 +8,33 @@ function App() {
     <>
     <Navigate/>
     </>
+=======
+import logo from './logo.svg';
+import './App.css';
+import Root from './Components/Root';
+import Partner from './Components/partner';
+import AppRoot from './Components/AppRoot';
+import Header from './Components/header';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <div className="header-align"></div>{/* add this with header */}
+      <Routes>
+        <Route path="/" element={<Root/>} />
+        <Route path="users" element={<AppRoot />} />
+        <Route path="partner" element={<Partner />} />
+      </Routes>
+    </BrowserRouter>
+>>>>>>> Stashed changes
   );
 }
 
