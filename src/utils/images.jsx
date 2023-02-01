169a10b0f18d65af784e32af66dd2c1bc120e1e7
Guna <guna6819@gmail.com>
2023-02-01 15:37:54 +0530

    home page and about us page

---
+++ src/utils/images.jsx
@@ -0,0 +1,98 @@
+import Logo from "../asset/image/Logo.png";
+import AsterNli from "../asset/image/aster-nli.png";
+import AsterPath from "../asset/image/aster-path.png";
+import Asterdonline from "../asset/image/asterdonline.png";
+// import BeautyIcon from "../asset/image/Beauty_Icon_02.png";
+import BlackDown from "../asset/image/black-down.png";
+import Call from "../asset/image/call.png";
+import CloseIcon from "../asset/image/close-icon.png";
+import DarkNext from "../asset/image/dark-next.png";
+import DarkPrew from "../asset/image/dark-prew.png";
+import Secure from "../asset/image/secure.png";
+import Online from "../asset/image/online.png";
+import CardOneImg from "../asset/image/banner-img-1.png";
+import CardTwoImg from "../asset/image/banner-img-2.png";
+import CardTwoImgApp from "../asset/image/Untitled_23.png";
+import FooterNewsOne from "../asset/image/footer_news.png";
+import FooterNewsTwo from "../asset/image/footer_news_two.png";
+import Location from "../asset/image/location-on.png";
+import Mail from "../asset/image/ionic-md-mail.png";
+import Phone from "../asset/image/feather-phone.png";
+import SendIcon from "../asset/image/SendIcon.png";
+import Linkedin from "../asset/image/Linkedin.png";
+import Facebook from "../asset/image/facebook.png";
+import Instagram from "../asset/image/instagram-2.png";
+import Youtube from "../asset/image/Youtube.png";
+import Twitter from "../asset/image/Twitter.png";
+import Whatsapp from "../asset/image/whatsapp.png";
+import ProductCategories1 from "../asset/image/product-categories-img-1.png";
+import ProductCategories2 from "../asset/image/product-categories-img-2.png";
+import ProductCategories3 from "../asset/image/product-categories-img-3.png";
+import ProductCategories4 from "../asset/image/product-categories-img-4.png";
+import HealthLibrary1 from "../asset/image/healthlibrary-img-1.png";
+import HealthLibrary2 from "../asset/image/healthlibrary-img-2.png";
+import HealthLibrary3 from "../asset/image/healthlibrary-img-3.png";
+import HealthLibrary4 from "../asset/image/healthlibrary-img-4.png";
+import OurServices1 from "../asset/image/ourservices-img-1.png";
+import OurServices2 from "../asset/image/ourservices-img-2.png";
+import OurServices3 from "../asset/image/ourservices-img-3.png";
+import OurServices4 from "../asset/image/ourservices-img-4.png";
+import CareBrands1 from "../asset/image/carebrands-img-1.png";
+import CareBrands2 from "../asset/image/carebrands-img-2.png";
+import CareBrands3 from "../asset/image/carebrands-img-3.png";
+import CareBrands4 from "../asset/image/carebrands-img-4.png";
+import CareBrands5 from "../asset/image/carebrands-img-5.png";
+import CareBrands6 from "../asset/image/carebrands-img-6.png";
+import CareBrands7 from "../asset/image/carebrands-img-7.png";
+import RetailOutlets1 from "../asset/image/retailoutlets-img-1.png";
+import RetailOutlets2 from "../asset/image/retailoutlets-img-2.png";
+import RetailOutlets3 from "../asset/image/retailoutlets-img-3.png";
+import LatestNews1 from "../asset/image/latestnews-img-1.png";
+import LatestNews2 from "../asset/image/latestnews-img-2.png";
+import ProductCategoriesIcon1 from "../asset/image/product-categories-img-1icon.png";
+import ProductCategoriesIcon2 from "../asset/image/product-categories-img-2icon.png";
+import ProductCategoriesIcon3 from "../asset/image/product-categories-img-3icon.png";
+import ProductCategoriesIcon4 from "../asset/image/product-categories-img-4icon.png";
+import AboutBanner from "../asset/image/aboutBanner.png";
+import Video from "../asset/image/homenew.mp4";
+
+export {
+  HealthLibrary1,
+  Logo,
+  AsterNli,
+  AsterPath,
+  Asterdonline,
+//   BeautyIcon,
+  BlackDown,
+  Call,
+  CloseIcon,
+  DarkNext,
+  DarkPrew,
+  Secure,
+  Online,
+  Video,
+  CardOneImg,
+  CardTwoImg,
+  CardTwoImgApp,
+  FooterNewsOne,
+  FooterNewsTwo,
+  Location,
+  Mail,
+  Phone,
+  SendIcon,
+  Linkedin,
+  Facebook,
+  Instagram,
+  Youtube,
+  Twitter,
+  Whatsapp,
+  OurServices1,
+  OurServices2,
+  OurServices3,
+  OurServices4,
+  ProductCategories1,
+  ProductCategories2,
+  ProductCategories3,
+  ProductCategories4,
+  ProductCategoriesIcon1,ProductCategoriesIcon2,ProductCategoriesIcon3,ProductCategoriesIcon4,AboutBanner
+};
