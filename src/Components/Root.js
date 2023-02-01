b34023ed3748c8f55163ff6458e7032eddef14dc
Ayush <Ayushmittal017@gmail.com>
2023-02-01 17:38:58 +0530

    all

---
+++ src/Components/Root.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import './Root.scss';
+import Header from './header';
+// import Menu from '@mui/material/Menu';
+// import MenuItem from '@mui/material/MenuItem';
+
+function Root() {
+    return (
+        <div className="App">
+        </div>
+    );
+}
+
+export default Root;
